1d41227b409aceb1fe2edb4a12c05fc959652738
deiji-merollin <elijahgalima7@gmail.com>
2025-10-20 16:48:41 +0800

    Galima-auth

--- app/firebase.js
+++ app/firebase.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAhb1Jmt_y6_3z0NbJVfdywo28ttJ7iwW8",
+  authDomain: "galima-auth.firebaseapp.com",
+  projectId: "galima-auth",
+  storageBucket: "galima-auth.firebasestorage.app",
+  messagingSenderId: "888917924635",
+  appId: "1:888917924635:web:6a79b16adbed99d4b9d1be",
+  measurementId: "G-EF5E8YY633"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
